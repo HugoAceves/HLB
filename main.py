758801a62e14e0fb9c6dd486bf0baf0c1f02e28d
Hugo Aceves <hugohaceves@gmail.com>
2021-09-20 01:14:54 -0500

    Commit del puente del 16

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
-
+import matplotlib
+import pylab
 #----------------------------------------- Clases ------------------------------------------
 class Tree:
 	def __init__(self,position,age=0.):
@@ -10,25 +11,18 @@ class Tree:
 		self.diaphorina_amount=0.
 
 		#Métodos
-	def infection(self):#Sortea si un árbol sano se infecta o no
-		if infected==False:#if el árbol está sano
+	def manual_infection(self):#Sortea si un árbol sano se infecta o no
+		if self.infected==False:#if el árbol está sano
 			try:
-				infection_probability=1.-diaphorina_amount**(-1./2.) #calculamos la probabilidad de contagio en función de las diaforinas
+				infection_probability=1.-diaphorina_amount**(-1.) #calculamos la probabilidad de contagio en función de las diaforinas
 			except:
-				infection_probability=0 #La probabilidad infección no depende precisamente del número de diaforinas, sino del número de diaforinas infectadas
+				infection_probability=0 #Estaa probabilidad infección depende sólo del número de diaforinas, no del número de diaforinas infectadas
 			if random.random()<infection_probability:#si un número aleatorio es menor que esa probabilidad, se contagia
 				infected=True
-class Diaphorina:
-	def __init__(self,tree=None):#tree=none es como pass para variables
-		self.tree=tree
-	
-		#Métodos
-	def flight_to(self,tree):#?
-		self.tree=tree#pass?
-	def random_flight(self,list_tree):#Elige aleatoriamente un elemento de la lista de árboles
-		self.tree=random.choice(list_tree)#Hay problema con que no haya sido definida aún esta lista?
+				self.positions_matrix[tree.position[0],tree.position[1]]=7
+				#Para una función más realista, véase infection() en la clase diaphorina
 class Field:
-	def __init__(self,size=(100,100)):
+	def __init__(self, size=(100,100)):
 		self.size=size
 		self.positions_matrix=np.zeros(size)#The function zeros creates an array full of zeros
 		forestlist_generator=[]
@@ -42,7 +36,10 @@ class Field:
 	def add_tree(self,tree):#Agrega un árbol en una posoción dada  
 		if self.positions_matrix[tree.position[0],tree.position[1]]==0:#-sólo si esta está vacía-
 			self.forestlist[tree.position[0]][tree.position[1]]=tree#al forestlist
-			self.positions_matrix[tree.position[0],tree.position[1]]=1#y ocupa la casilla en positions_matrix.
+			if tree.infected==True:#y ocupa la casilla en positions_matrix con 7 if infected y con 1 if not
+				self.positions_matrix[tree.position[0],tree.position[1]]=7
+			else:
+				self.positions_matrix[tree.position[0],tree.position[1]]=1
 		else:#Si no está vacía, entonces pass. Esto para evitar borrar accidentalmente
 			pass#Se hará un código para el método replace_tree() y para delete_tree().
 	def delete_tree(self, position, tree=None): 
@@ -57,15 +54,20 @@ class Field:
 			self.delete_tree(old_tree)
 			self.add_tree(new_tree)'''
 	
-	def fill(self, age, diaphorina_amount, infected=False):#Nos llena el campo con árboles aleatorios
+	def fill(self, age, diaphorina=False, infected=False):#Nos llena el campo con árboles aleatorios
 		for i in range(self.size[0]):
 			for j in range(self.size[1]):
-				random_tree=Tree((i,j), age)
-				random_tree.diaphorina_amount=diaphorina_amount
+				random_tree=Tree((i,j), age) #Crea el árbol ijotaésimo
+				if diaphorina==False:#Si no se pide que el árbol tenga diaforinas
+					random_tree.diaphorina_amount=0 # Le asocia 0 diaforinas
+				else:#Si se pide que tenga
+					random_tree.diaphorina_amount=random.randint(300, 400) #Se le ponen entre 300 y 400
+
 				if infected==True:
 					random_tree.infection()
-				self.forestlist[i][j]=random_tree
-				self.positions_matrix[i,j]=1
+
+				self.forestlist[i][j]=random_tree #lo inserta en el nosque
+				self.positions_matrix[i,j]=1 #Lo marca en la matriz de posociones
 				'''print(self.forestlist[i][j])
 				print(self.positions_matrix[i,j])'''
 	'''En construcción: def add_random_trees(self,Number_of_trees):
@@ -78,6 +80,23 @@ class Field:
 	Quité momentáneamente este método porque los campos comerciales no tienen  árboles distribuidos aleatoriamente, 
 	están siempre en cuadrículas llenas, quizás en los traspatios sí lo estén, 
 	'''
+class Diaphorina:
+	def __init__(self, field, position=(None, None)):
+		self.position=position
+
+		#Métodos
+		#-----------Lo importante es programar un condicional al vuelo de la diaforina
+		#Una diaforina no s queda en un árbol si no se cumple cierto coeficiente de comodidad, y vuela aleatriamente hasta encontrarlo
+		#este coeficiente podrá depender de muchos factores
+
+
+	def flight_to(self, position=(None, None)):#Mover una diaforina de un punto a otro particular
+		if position!=(None, None):
+			pass
+		else:
+			self.position=position
+	def random_flight(self,list_tree):#Mover una diaforina de un punto a otro aleatorio de la lista de árboles
+		self.position=(random.randint(0, field.size[0]-1), random.randint(0, field.size[1]-1))#Hay problema con que no haya sido definida aún esta lista?
 #-------------------------- Creación de Objetos --------------------------------
 
 #Campo uno y métodos básicos
@@ -121,7 +140,7 @@ print(field_1.positions_matrix)
 '''
 
 #Campo dos y fill
-field_2=Field((10, 10))
+'''field_2=Field((10, 10))
 print("Forestlist -Campo2 virgen-")
 print(field_2.forestlist)
 print("")
@@ -139,6 +158,38 @@ print("")
 print("Posiciones -Campo2 filled-")
 print(field_2.positions_matrix)
 print("")
+'''
+field_3=Field((20,10))
+diaforina_1=Diaphorina(field_3)
+tree_1=Tree((0, 4))
+tree_2=Tree((2, 3))
+tree_3=Tree((3, 5))
+field_3.add_tree(tree_1)
+field_3.add_tree(tree_2)
+field_3.add_tree(tree_3)
+tree_1.diaphorina_amount=100000
+tree_1.manual_infection()
+tree_2.diaphorina_amount=100000
+tree_2.manual_infection()
+tree_3.diaphorina_amount=100000
+tree_3.manual_infection()
 
+print("Forestlist")
+print(field_3.forestlist)
+print("")
 
-#Hacer que si un árbol está infectado se vea como un 7 en position matrix
+print("Posiciones")
+print(field_3.positions_matrix)
+print("")
+
+#Commit 19/9/21:
+#-Completado	Definir la clase diaforina
+
+#En proceso:
+#Hacer que si un árbol está infectado se vea como un 7 en position matrix (no se muestra ningún 7 luego de usar manual_infection)
+
+#Pendientes:
+#Crear una diaphorina_matrix que nos diga el número de diaforinas que hay 
+#Hacer que al crear un árbol, se creen los objetos diaforina en él si es que diaphorina_amount!=0
+#Dar una forma de distribuir las diaforinas con el modelo hecho en el capítulo 19
+#Hacer algo para verlo gráficamente
